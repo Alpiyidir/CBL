a7050d9c7a68bf90903347f6e8a068edb70fbe98
Alp Solakoglu <solakoglualp@yahoo.com>
2023-10-20 07:31:58 +0200

    Added aim cone to bullet trajectors so it's not 100% accurate

--- Bullet.java
+++ Bullet.java
@@ -2,4 +2,17 @@ public class Bullet extends CircularObjectWithUpdate {
     public Bullet(double xPos, double yPos, double speed, double radius, double[] normalizedDirectionVector) {
         super(xPos, yPos, speed, radius, normalizedDirectionVector);
     }
+
+    // Adds aim cone to bullet
+    @Override
+    public void setNormalizedDirectionVector(double[] normalizedDirectionVector) {
+        double[] normalVector = new double[]{normalizedDirectionVector[1], -normalizedDirectionVector[0]};
+        double degree = Math.PI / 180;
+        double offsetMultiplierPerUnitLength = Math.tan(2 * degree);
+        double offsetDirection = Math.random() * 2 - 1;
+        normalVector[0] *= offsetMultiplierPerUnitLength * offsetDirection;
+        normalVector[1] *= offsetMultiplierPerUnitLength * offsetDirection;
+
+        super.setNormalizedDirectionVector(MathHelpers.normalizeVector(MathHelpers.sumVectors(normalizedDirectionVector, normalVector)));
+    }
 }
--- BulletWithAimCone.java
+++
@@ -1,5 +0,0 @@
-public class BulletWithAimCone extends Bullet {
-    public BulletWithAimCone(double xPos, double yPos, double speed, double radius, double[] directionVector) {
-        super(xPos, yPos, speed, radius, directionVector);
-    }
-}
