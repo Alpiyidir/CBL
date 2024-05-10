de700ee08e0e0195069567ba851ae68d8201684d
Jeremi Machniewski <jeremi.machniewski@gmail.com>
2024-05-10 14:59:28 +0200

    Added second test

---
+++ Downloads/candy_skeleton/src/test/java/CandyTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A JUnit test case class.
+ * Every method starting with the word "test" will be called when running
+ * the test with JUnit.
+ *
+<!--//# BEGIN TODO: Name, student ID, and date-->
+<p><b>Jeremi Machniewski, 1942662, 10-5-2024</b></p>
+<!--//# END TODO-->
+ */
+public class CandyTest {
+
+    static final Candy SUT = null; // to simplify method calls
+
+    static final long MAX_VALUE = 999999999999999999L;
+
+    /**
+     * Checks the result of SUT.divide(k, c).
+     */
+    private void check(long k, long c, boolean expected) {
+        System.out.println("divide(" + k + ", " + c + ")");
+        long result = SUT.divide(k, c);
+        System.out.println("  result = " + result);
+        assertEquals(expected, 0 <= result, "possible (0 <= result)");
+        if (0 <= result) {
+            assertTrue(result <= MAX_VALUE, "range (result <= MAX_VALUE)");
+            assertEquals(result * k, c, "quotient (result * k == c)");
+        }
+    }
+
+    // Test cases
+
+    /** The given example. */
+    @Test
+    public void testDivideGivenExample() {
+        check(3, 15, true);
+    }
+
+    //# BEGIN TODO: Additional test cases
+    public void testDivideNotWork() {
+        check(3, 16, false);
+    }
+    //# END TODO
+
+}
